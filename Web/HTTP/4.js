b472b9918b7560e63520b0607f3234f134dfcbde
allenalvin333 <allenalvin333@gmail.com>
2021-09-03 21:21:15 +0530

    programs

---
+++ Web/HTTP/4.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.delete('/check/:id',(req,res)=>{
+    res.json({
+        msg:`${req.params.id} deleted`
+    });
+});
+app.listen(5000,()=>console.log(`Server started on 5000`));
