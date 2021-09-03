18db8285189ad278016b11d03f3ab80e983f4d25
allenalvin333 <allenalvin333@gmail.com>
2021-09-03 22:27:51 +0530

    programs

--- Web/HTTP/1.js
+++ Web/HTTP/1.js
@@ -1,3 +1,4 @@
+// Using ExpressJS
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -5,6 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// GET request to the root of the server
 app.get('/',(req,res)=>{
+    // Display host parameter of the HTTP request header
     res.send(req.header('host'));});
+// Log the message to console if the result appears on port 5000
 app.listen(5000,()=>console.log(`Server started on 5000`));
