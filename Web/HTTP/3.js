a93c5ee0bced69c3a138ee4201ae747aaf440685
allenalvin333 <allenalvin333@gmail.com>
2021-09-03 21:12:12 +0530

    programs

--- Web/HTTP/3.js
+++ Web/HTTP/3.js
@@ -5,21 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/contact',(req,res)=>{
-    if(!req.body.name) {
-        return res.status(400).send('Name is required');
-    }
-    res.status(201).send(`Thank You ${req.body.name}`);
-});
-
-app.post('/login',(req,res)=>{
-    if(!req.header('x-auth-token')) {
-        return res.status(400).send('No Token');
-    }
-    if(req.header('x-auth-token')!=='12345') {
-        return res.status(401).send('Not Authorized');
-    }
-    res.send(`Authorized`);
+app.put('/post/:id',(req,res)=>{
+    res.json({
+        id: req.params.id,
+        title: req.body.title
+    });
 });
 
 app.listen(5000,()=>console.log(`Server started on 5000`));
