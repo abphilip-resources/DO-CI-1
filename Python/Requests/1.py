95af89a2d6168902a7c8b16237d729be70b8a0a3
allenalvin333 <allenalvin333@gmail.com>
2021-09-04 19:13:14 +0530

    programs

--- Python/Requests/1.py
+++ Python/Requests/1.py
@@ -1,3 +1,7 @@
 import requests
 
-print("HI")
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid=e474d2b59f05f48dd4689975c9c62471')
+# Print available methods   -->     print(dir(r))
+# Print Guide for methods   -->     print(help(r))
+
+print(r.text)
