214921a1aef2756d333f815849a1e539474128d9
allenalvin333 <allenalvin333@gmail.com>
2021-09-06 16:59:32 +0530

    programs

--- Python/Requests/2.py
+++ Python/Requests/2.py
@@ -14,11 +14,11 @@ r = s.get('https://httpbin.org/cookies')
 print("\n",r.json()['cookies'])
 r.close()
 
-# JSON
-# r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid={}'.format(api))
-# for k1, v1 in r.json().items(): 
-#     if(k1=='main' or k1=='coord' or k1=='sys'):
-#         for k2, v2 in v1.items(): print(k2, ":", v2)
-#     else: print(k1, ":", v1)
+# JSON using API
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid={}'.format(api))
+for k1, v1 in r.json().items(): 
+    if(k1=='main' or k1=='coord' or k1=='sys'):
+        for k2, v2 in v1.items(): print(k2, ":", v2)
+    else: print(k1, ":", v1)
 
-response = requests.get('https://api.covid19api.com/summary').text
+# https://realpython.com/api-integration-in-python/
