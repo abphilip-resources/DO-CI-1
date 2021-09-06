d7e0bdd8e9e5c8472a22086a0851fd56e795fb68
allenalvin333 <allenalvin333@gmail.com>
2021-09-06 16:28:31 +0530

    programs

--- Python/Requests/2.py
+++ Python/Requests/2.py
@@ -1,18 +1,24 @@
 import os
+import json
 import requests
+
+# Loading variable from .env
 from dotenv import load_dotenv
-load_dotenv() 
+load_dotenv()
+api=os.getenv('API_KEY')
 
 # Create a session object
 s = requests.Session()
 s.get('https://httpbin.org/cookies/set/sessioncookie/allen')
 r = s.get('https://httpbin.org/cookies')
 print("\n",r.json()['cookies'])
-
-# Authentication via API Key in .env
-api=os.getenv('API_KEY')
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid={}'.format(api))
-print(r.json())
 r.close()
 
-# Parsing a JSON response 
+# JSON
+# r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid={}'.format(api))
+# for k1, v1 in r.json().items(): 
+#     if(k1=='main' or k1=='coord' or k1=='sys'):
+#         for k2, v2 in v1.items(): print(k2, ":", v2)
+#     else: print(k1, ":", v1)
+
+response = requests.get('https://api.covid19api.com/summary').text
