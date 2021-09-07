b2bfc4590c6c2c1f98cd48554e717b89f5101cc5
allenalvin333 <allenalvin333@gmail.com>
2021-09-07 11:23:30 +0530

    programs

--- Python/Requests/1.py
+++ Python/Requests/1.py
@@ -30,25 +30,25 @@ r.close()
 data = {'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': True}
 r = requests.put("https://jsonplaceholder.typicode.com/todos/1",json=data)
 print(r.json())
-print("HTTP Status Code:",r.status_code,"\n")      # 201 if added
+print("HTTP Status Code:",r.status_code,"\n")      # 200 if modified
 r.close()
 
 # PATCH Method              --> Modify value of a specific fields in a resource
 data = {'title': 'Clean Garage', 'completed': True}
 r = requests.patch("https://jsonplaceholder.typicode.com/todos/1",json=data)
 print(r.json())
-print("HTTP Status Code:",r.status_code,"\n")      # 201 if added
+print("HTTP Status Code:",r.status_code,"\n")      # 200 if modified
 r.close()
 
 # DELETE Method             --> Delete existing resources in server
 r = requests.delete("https://jsonplaceholder.typicode.com/todos/1",json=data)
 print(r.json())
-print("HTTP Status Code:",r.status_code,"\n")      # 201 if added
+print("HTTP Status Code:",r.status_code,"\n")      # 200 if deleted
 r.close()
 
 # Real World Application of Requests
 r = requests.get('https://api.github.com/users/allenalvin333')
 print("Working at:", r.json()['company'])
 print("Website:", r.json()['blog'])
-print("HTTP Status Code:",r.status_code)           # 200 if successful
+print("HTTP Status Code:",r.status_code,"\n")      # 200 if successful
 r.close()
