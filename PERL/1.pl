50b2eeffaad7aaef07994f6fd538f66d209b7802
allenalvin333 <allenalvin333@gmail.com>
2021-09-07 20:12:48 +0530

    programs

--- PERL/1.pl
+++ PERL/1.pl
@@ -6,8 +6,24 @@ use feature 'say';                                      # Print Function Enabled
 use feature 'switch';                                   # Switch Case Enabled
 use v5.16;                                              # Perl Version Fixed
 
-print "Hello World\n"
+print "Hello World\n";
 
-my $name = "Allen";                                     # String with variable name (scalar)
+my $name = 'Allen';                                     # String with variable name (scalar)
 my $age = 20;                                           # Integer with variable age (scalar)
-my ($street,$city) = ("Indiranagar","Bangalore");       # String with variable address (scalar)
+my ($street,$city) = ('Indiranagar','Bangalore');       # String with Multiple Values (Array)
+
+my $info = "$name lives on \"$street\"";                # Double quotes for additional functionality
+$info = qq{$name lives on "$street"};                   # Same as the previous line
+
+my $info2 = <<"END";                                    # String with Multiline
+This is a lot 
+of information
+on multiple lines
+END
+
+say $info2
+
+my $f1 = 1.00096;
+my $f2 = 8.12073;
+
+print("$f1 + $f2 = ",$f1+$f2,"\n");
