19e4eae70a2863414c70d7cea4fb8a5e7a55f4af
allenalvin333 <allenalvin333@gmail.com>
2021-09-07 19:15:49 +0530

    programs

---
+++ PERL/1.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';                                      # Print Function Enabled
+use feature 'switch';                                   # Switch Case Enabled
+use v5.16;                                              # Perl Version Fixed
+
+print "Hello World\n"
+
+my $name = "Allen";                                     # String with variable name (scalar)
+my $age = 20;                                           # Integer with variable age (scalar)
+my ($street,$city) = ("Indiranagar","Bangalore");       # String with variable address (scalar)
