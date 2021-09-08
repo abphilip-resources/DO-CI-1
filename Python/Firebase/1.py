c2fa2cb2a1dbc94595685b0ec89dff1134dd28f0
allenalvin333 <allenalvin333@gmail.com>
2021-09-08 18:13:22 +0530

    programs

---
+++ Python/Firebase/1.py
@@ -0,0 +1,47 @@
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+from dotenv import load_dotenv
+load_dotenv()
+p1=os.getenv('private_key_id')
+p2=os.getenv('private_key')
+p3=os.getenv('client_id')
+u1=os.getenv('client_x509_cert_url')
+u2=os.getenv('databaseURL')
+
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": "learning-290101",
+  "private_key_id": p1,
+  "private_key": p2,
+  "client_email": "firebase-adminsdk-x0tar@learning-290101.iam.gserviceaccount.com",
+  "client_id": p3,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": u1
+})
+firebase_admin.initialize_app(cred, {'databaseURL': u2})
+
+ref = db.reference('py/')
+users_ref = ref.child('users')
+users_ref.set({
+    'Allen': {
+        'date_of_birth': 'September 26, 2000',
+        'full_name': 'Allen Ben Philipose'
+    },
+    'Christi': {
+        'date_of_birth': 'January 29, 2001',
+        'full_name': 'Christiana John'
+    }
+})
+
+hopper_ref = users_ref.child('Christi')
+hopper_ref.update({
+    'nickname': 'Kishti Mol'
+})
+
+handle = users_ref.child('py/users/alanisawesome').get()
+print(ref.get())
