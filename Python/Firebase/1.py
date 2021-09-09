35b3480144956aa402c30da276b6eac4efab0d96
allenalvin333 <allenalvin333@gmail.com>
2021-09-09 11:39:52 +0530

    programs

--- Python/Firebase/1.py
+++ Python/Firebase/1.py
@@ -1,4 +1,5 @@
 import os
+import firebase
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -14,27 +15,27 @@ u2=os.getenv('databaseURL')
 
 # Creating credentials certificate for Authorization
 cred = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "learning-290101",
-  "private_key_id": p1,
-  "private_key": p2,
-  "client_email": "firebase-adminsdk-x0tar@learning-290101.iam.gserviceaccount.com",
-  "client_id": p3,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": u1
+    "type": "service_account",
+    "project_id": "learning-290101",
+    "private_key_id": p1,
+    "private_key": p2,
+    "client_email": "firebase-adminsdk-x0tar@learning-290101.iam.gserviceaccount.com",
+    "client_id": p3,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": u1
 })
 
 # Initializing the app with the certificate and access URL
 firebase_admin.initialize_app(cred, {'databaseURL': u2})
 
-# CRUD Operations on DB --> Create, Read, Update, Delete
+# CRUD Operations on DB         --> Create, Read, Update, Delete
 
-# Create
-ref = db.reference('')
-users_ref = ref.child('users')
-users_ref.set({
+# Create                        --> .set()
+r = db.reference('')
+users = r.child('users')
+users.set({
     'Allen': {
         'date_of_birth': 'September 26, 2000',
         'full_name': 'Allen Ben Philipose'
@@ -50,19 +51,19 @@ users_ref.set({
     }
 })
 
-# Read
-handle = db.reference('users/Christi')
-print(handle.get())
+# Read                          --> .get()
+read = r.child('users').child('Christi')
+print(read.get())
 
-# Update
-hopper_ref = users_ref.child('Christi')
-hopper_ref.update({'nick_name': 'Kishti Mol'})
-hopper_ref = users_ref.child('Alvin')
-hopper_ref.update({'nick_   name': 'Alvin Boo'})
+# Update                        --> .update()
+update = r.child('users').child('Christi')
+update.update({'nick_name': 'Kishti Mol'})
+update = r.child('users').child('Alvin')
+update.update({'nick_name': 'Alvin Boo'})
 
-# Delete
-#https://www.youtube.com/watch?v=s-Ga8c3toVY
-#https://www.youtube.com/watch?v=rKuGCQda_Qo
-#https://www.youtube.com/watch?v=LaGYxQWYmmc
+# Delete                        --> .delete()
+delete = r.child('users').child('Allen')
+delete.delete()
 
-#https://console.firebase.google.com/u/0/project/learning-290101/database/learning-290101-default-rtdb/data
+# https://www.youtube.com/watch?v=s-Ga8c3toVY
+# https://www.youtube.com/watch?v=LaGYxQWYmmc
