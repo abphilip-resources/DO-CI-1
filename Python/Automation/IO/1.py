9d5a797e8b74a92b6520ed439137c3f6b31ccb56
allenalvin333 <allenalvin333@gmail.com>
2021-11-02 20:42:12 +0530

    programs

--- Python/Automation/IO/1.py
+++ Python/Automation/IO/1.py
@@ -1,6 +1,6 @@
-F = open("File.txt",'r')                                # Open file in read mode
-passFile = open("Pass.txt",'w')                         # Create file in write mode
-failFile = open("Fail.txt",'w')                         # Create file in write mode
+F = open("file.txt",'r')                                # Open file in read mode
+passFile = open("pass.txt",'w')                         # Create file in write mode
+failFile = open("fail.txt",'w')                         # Create file in write mode
 for line in F:
     if(line.split()[2]=="P"): passFile.write(line)      # Write the line to passFile
     else: failFile.write(line)                          # Write the line to failFile    
