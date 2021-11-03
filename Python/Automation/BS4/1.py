9ab021e176bcd1c13e17832f2698c6b548f8c5a8
allenalvin333 <allenalvin333@gmail.com>
2021-11-03 08:22:27 +0530

    programs

--- Python/Automation/BS4/1.py
+++ Python/Automation/BS4/1.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'http://quotes.toscrape.com/'                     # URL to scrape from
 response = requests.get(url)                            # Send a request to the website
-soup = BeautifulSoup(response.text,'lxml')              # Create a BeautifulSoup object
+soup = BeautifulSoup(response.text, 'lxml')             # Create a BeautifulSoup object
 
 q = soup.find_all("span", class_="text")                # Find all the quotes from span with class text
 a = soup.find_all("small", class_="author")             # Find all the authors from small with class author
@@ -12,6 +12,6 @@ t = soup.find_all("div", class_="tags")                 # Find all the tags from
 for z in range(len(q)):
     print('{}.'.format(z+1), q[z].text)                 # Print the quotes
     print('-', a[z].text, end=' || ')                   # Print the authors
-    tags = t[z].find_all('a',class_='tag')              # Find all the tags from a with class tag
+    tags = t[z].find_all('a', class_='tag')             # Find all the tags from a with class tag
     for y in tags: print(y.text, end='. ')              # Print the tags
     print("\n")
