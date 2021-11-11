21d1f607766e150f0c02570fd820baf9e12fa7aa
allenalvin333 <allenalvin333@gmail.com>
2021-11-11 10:28:27 +0530

    programs

--- Python/Decorators/app.py
+++ Python/Decorators/app.py
@@ -8,18 +8,18 @@ def home():
 
 @app.route('/a')                                        # Decorator for showing the a page path
 def a():
-    return '''<h1>Allen</h1><br/><br/>                                
+    return '''<h1>Allen</h1><br/>
     <h2>Contact me</h2>
     <b>9449277201, allenbphilip@gmail.com</b>'''        # Return the html code
 
 @app.route('/b')                                        # Decorator for showing the b page path
 def b():
-    return '''<h1>Allen</h1><br/><br/>                                
+    return '''<h1>Allen</h1><br/>
     <h2>Occupation</h2>
     <i>Data Analyst at ZS</i>'''                        # Return the html code
 
 @app.route('/c')                                        # Decorator for showing the c page path
 def c():
-    return '''<h1>Allen</h1><br/><br/>                                
+    return '''<h1>Allen</h1><br/>
     <h2>Website</h2>
     <a href="https://abphilip.me">Click Here</b>'''     # Return the html code
