d663e44e627471d7dcba12da4883134476c6f982
allenalvin333 <allenalvin333@gmail.com>
2021-11-14 09:13:22 +0530

    programs

--- Web/API/1/4.js
+++ Web/API/1/4.js
@@ -7,6 +7,10 @@ request.open('GET', URL);							    // Open Request
 request.onload = function() {						    // Action once data received
 	data = JSON.parse(request.response)					// Parse JSON
 	console.log(data);	        						// Log Response
-	
+	console.log(data[0].name);							// Access Data via console
+
+	page = document.createElement('li');				// Create Element
+	page.innerHTML = data[0].name;						// Set Element Content
+	document.body.appendChild(page);					// Append Element to Body
 };
 request.send()                                          // Send Request     
