b818c4cf0b76270de576a2ed275e65e437a77dae
allenalvin333 <allenalvin333@gmail.com>
2021-11-15 13:01:25 +0530

    programs

--- Python/Flask/2/1.py
+++ Python/Flask/2/1.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import url_for
+from flask import request
+from flask import redirect
 from flask import render_template
 
 app = Flask(__name__)
@@ -11,6 +14,13 @@ def home():
 def about():
     return 'URL Shortener'
 
-@app.route('/your-url')
+@app.route('/your-url', methods=['POST','GET'])
 def your_url():
-    return render_template('your_url.html', code=request.args['code'])
+    if(request.method=='POST'):
+        urls = {}
+        urls[request.form['code']] = {'url':request.form['url']}
+        with open('urls.json','w') as url_file:
+            json.dump(urls, url_file)
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('home'))
