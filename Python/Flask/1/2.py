40d86312423e889214259eea30c87fe08bd002b5
allenalvin333 <allenalvin333@gmail.com>
2021-11-16 22:13:57 +0530

    programs

--- Python/Flask/1/2.py
+++ Python/Flask/1/2.py
@@ -1,21 +1,20 @@
-from flask import Flask, render_template, request, flash, jsonify, redirect
+from flask import Flask, render_template, flash
+from flask import request, jsonify, redirect
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Allen's App using Flask"
+    return "<h1> Allen's App using Flask <h1>"
 
 @app.route('/alerts/create')
 def create():
-    try:
-        return jsonify({'message':'Alert created successfully'})
+    try: return jsonify({'message':'Alert created successfully'})
     except: return jsonify({'message':'Error creating alert'})
 
 @app.route('/alerts/delete')
 def delete():
-    try:
-        return jsonify({'message':'Alert deleted successfully'})
+    try: return jsonify({'message':'Alert deleted successfully'})
     except: return jsonify({'message':'Error deleting alert'})
 
 if __name__ == "__main__":
