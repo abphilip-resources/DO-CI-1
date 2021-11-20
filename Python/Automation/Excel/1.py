43a9088e9a9a372e123864ddb06bf3194c9110d6
allenalvin333 <allenalvin333@gmail.com>
2021-11-20 20:57:18 +0530

    programs

--- Python/Automation/Excel/1.py
+++ Python/Automation/Excel/1.py
@@ -7,3 +7,6 @@ df_csv = pd.read_csv("data/read/ReadCSV.csv", header=None)
 
 df_csv.columns = ["First", "Last", "Address", "City", "State", "Zip", "Amount"]
 df_csv.to_excel("data/read/WriteExcel.xlsx", index=False)
+
+print(df_csv[["First", "Zip"]])
+print(df_csv.loc[0:1, ["State"]])
