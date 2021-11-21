ad081726df9ce49cff16ddda0288b395ae68c15b
allenalvin333 <allenalvin333@gmail.com>
2021-11-21 22:23:14 +0530

    programs

--- Python/Automation/Excel/3.py
+++ Python/Automation/Excel/3.py
@@ -7,4 +7,13 @@ from openpyxl.styles import GradientFill, colors
 loc = 'C:/Users/allen/OneDrive/Desktop/Github/Learning/' \
       'Python/Automation/Excel/data/2'                          # Location of the directory
 wb1 = Workbook()                                                # Create workbook
-ws1 = wb1.active                                                # Go to active sheet of wb1
+ws1 = wb1.active                                                # Go to active sheet of 
+
+for z in range(1,20): ws.append(range(300))
+
+ws.merge_cells('A1:B5')                                         # Merge cells A1:B5
+ws.unmerge_cells('A1:B5')                                       # Unmerge cells A1:B5
+ws.merge_cells(
+    start_row=1, start_column=1, 
+    end_row=5, end_column=2
+)                                                               # Merge cells A1:B5 
