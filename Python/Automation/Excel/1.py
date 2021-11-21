980ecedbff7b9e70093032d69d96060351f3d258
allenalvin333 <allenalvin333@gmail.com>
2021-11-21 11:08:59 +0530

    programs

--- Python/Automation/Excel/1.py
+++ Python/Automation/Excel/1.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from openpyxl.workbook import Workbook
 loc = "C:/Users/allen/OneDrive/Desktop/Github/Learning/" \
-      "Python/Automation/Excel/data/read"
+      "Python/Automation/Excel/data/read"                                   # Location of the directory
 
-df_excel = pd.read_excel(f"{loc}/ReadExcel.xlsx")
-df_text = pd.read_csv(f"{loc}/ReadText.txt", delimiter="/t")
-df_csv = pd.read_csv(f"{loc}/ReadCSV.csv", header=None)
+df_excel = pd.read_excel(f"{loc}/ReadExcel.xlsx")                           # Read the excel file
+df_text = pd.read_csv(f"{loc}/ReadText.txt", delimiter="/t")                # Read the text file
+df_csv = pd.read_csv(f"{loc}/ReadCSV.csv", header=None)                     # Read the csv file
 
-df_csv.columns = ["First", "Last", "Address", "City", "State", "Zip", "Amount"]
-df_csv.to_excel(f"{loc}/WriteExcel.xlsx", index=False)
+df_csv.columns = ["First", "Last", "Address", "City", "State", "Zip"]       # Rename the columns
+df_csv.to_excel(f"{loc}/WriteExcel.xlsx", index=False)                      # Write the csv file to excel
 
-print(df_csv[["First", "Zip"]], end="\n\n")
-print(df_csv["City"][0:4], end="\n\n")
-print(df_csv.iloc[1,1])
+print(df_csv[["Last", "Zip"]], end="\n\n")                                  # Print the last name and zip code columns
+print(df_csv["City"][0:4], end="\n\n")                                      # Print the first 4 cities
+print(df_csv.iloc[1,1], end="\n\n")                                         # Print the second row, second column
+print(df_csv.loc[(df_csv["First"]=="John") & 
+     (df_csv["City"]=="Riverside")], end="\n\n")                            # Print the rows where first name is John and city is Riverside
