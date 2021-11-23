fcef597c57ef090d608508c7bd2bb8af2bdc5658
allenalvin333 <allenalvin333@gmail.com>
2021-11-23 09:45:48 +0530

    programs

--- Python/Automation/Excel/4.py
+++ Python/Automation/Excel/4.py
@@ -3,7 +3,7 @@ from PIL import Image
 from openpyxl import load_workbook
 from openpyxl.workbook import Workbook
 from openpyxl.chart import PieChart, PieChart3D
-from openpyxl.chart import Reference, Series
+from openpyxl.chart import Reference, Series, BarChart
 from openpyxl.worksheet.table import Table, TableStyleInfo
 from openpyxl.drawing.image import Image
 loc = 'C:/Users/allen/OneDrive/Desktop/Github/Learning/' \
@@ -22,13 +22,27 @@ data = [
 ]                                                               # Data to be written
 for z in data: ws.append(z)                                     # Write data to the sheet
 
-# Chart
-chart = PieChart()                                              # Create chart
-chart.title = 'Ice_Cream_Chart'                                 # Title of the chart
+# Pie Chart
+chart1 = PieChart()                                             # Create chart
+chart1.title = 'Ice_Cream_Chart_Pie'                            # Title of the chart
 labels = Reference(ws, min_col=1, min_row=2, max_row=7)         # Labels of the chart
 data = Reference(ws, min_col=2, min_row=1, max_row=7)           # Data of the chart
-chart.add_data(data, titles_from_data=True)                     # Add data to the chart
-chart.set_categories(labels)                                    # Set labels of the chart
+chart1.add_data(data, titles_from_data=True)                    # Add data to the chart
+chart1.set_categories(labels)                                   # Set labels of the chart
+chart1.height = 7.5                                             # Height of the chart
+chart1.width = 10                                               # Width of the chart
+chart1.style = 42                                               # Style of the chart
+
+# Bar Chart
+chart2 = BarChart()                                             # Create chart
+chart2.title = 'Ice_Cream_Chart_Bar'                            # Title of the chart
+labels = Reference(ws, min_col=1, min_row=2, max_row=7)         # Labels of the chart
+data = Reference(ws, min_col=2, min_row=1, max_row=7)           # Data of the chart
+chart2.add_data(data, titles_from_data=True)                    # Add data to the chart
+chart2.set_categories(labels)                                   # Set labels of the chart
+chart2.height = 7.5                                             # Height of the chart
+chart2.width = 15                                               # Width of the chart
+chart2.style = 42                                               # Style of the chart
 
 # Table
 tab = Table(displayName='Ice_Cream_Table', ref='A1:B7')         # Create table
@@ -39,12 +53,13 @@ tab.tableStyleInfo = TableStyleInfo(name='TableStyleMedium10',  # Name of the st
 
 # Image
 img = Image(f'{loc}/C_Image.png')                               # Create image
-img.width = img.width * 0.71                                    # Width of the image
-img.height = img.height * 0.71                                  # Height of the image
+img.width = img.width * 0.32                                    # Width of the image
+img.height = img.height * 0.32                                  # Height of the image
 
 
 # Save
-ws.add_chart(chart, 'D1')                                       # Add chart to the sheet
+ws.add_chart(chart1, 'D1')                                      # Add chart to the sheet
+ws.add_chart(chart2, 'K1')                                      # Add chart to the sheet
 ws.add_table(tab)                                               # Add table to the sheet
-ws.add_image(img, 'N1')                                         # Add image to the sheet
+ws.add_image(img, 'A9')                                         # Add image to the sheet
 wb.save(f'{loc}/C_Write.xlsx')                                  # Save the workbook
